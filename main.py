5b4b4c625e7212b668dfab3538560e28b49f67a8
DISK131 <dada.00131@gmail.com>
2021-09-27 20:51:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 # 商品クラス
 class Item:
-    def __init__(self,item_code,item_name,price):
-        self.item_code=item_code
-        self.item_name=item_name
-        self.price=price
-    
+    def __init__(self, item_code, item_name, price):
+        self.item_code = item_code
+        self.item_name = item_name
+        self.price = price
+
     def get_price(self):
         return self.price
 
+
 # オーダークラス
 class Order:
-    def __init__(self,item_master):
-        self.item_order_list=[]
-        self.item_master=item_master
-    
-    def add_item_order(self,item_code):
+    def __init__(self, item_master):
+        self.item_order_list = []
+        self.item_master = item_master
+
+    def add_item_order(self, item_code):
         self.item_order_list.append(item_code)
-        
+
     def view_item_list(self):
         for item in self.item_order_list:
-            print("商品コード:{}".format(item))
-    
+            print(f"商品コード:{item}")
+
 
 def main():
     # マスタ登録
-    item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
-    
+    item_master = []
+    item_master.append(Item("001", "りんご", 100))
+    item_master.append(Item("002", "なし", 120))
+    item_master.append(Item("003", "みかん", 150))
+
     # オーダー登録
-    order=Order(item_master)
-    
+    order = Order(item_master)
+
     # 注文
     order.add_item_order("001")
     order.add_item_order("002")
     order.add_item_order("003")
-    
+
     # オーダー表示
     order.view_item_list()
-    
+
+
 if __name__ == "__main__":
     main()
